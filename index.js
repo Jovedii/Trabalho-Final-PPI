c02f46ecf83a43959096f8fe182236c40bdabbcb
Jovedii <jovedigui@gmail.com>
2024-06-15 18:12:02 -0300

    salvando

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ import cookieParser from 'cookie-parser';
 const host='0.0.0.0';
 const porta=3000;
 
-let animais[];
+let animais= [];
+let usuarioAutenticado= false;
 
 const app=express();
 app.use(session({
@@ -21,6 +22,16 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended: true}));
 
 app.use(express.static(path.join(process.cwd(), 'publico')));
+app.use(usuarioEstaAutenticado,express.static(path.join(process.cwd(), 'protegido')));
+
+function usuarioEstaAutenticado(requisicao, resposta, next){
+    if(usuarioAutenticado){
+        next();
+    }
+    else{
+        resposta.redirect('login.html');
+    }
+}
 
 app.listen(porta, host,() => {
     console.log(`O servidor está executando em https://${host}:${porta}`);
