5c72e1fbe5d6db31791a0d838abc38eb56fddef0
Jovedii <jovedigui@gmail.com>
2024-06-15 17:41:12 -0300

    salvando parte 2

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import session from 'express-session';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+
+const host='0.0.0.0';
+const porta=3000;
+
+let animais[];
+
+const app=express();
+app.use(session({
+    secret: '2305',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000*60*30
+    }
+}));
+app.use(cookieParser());
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static(path.join(process.cwd(), 'publico')));
+
+app.listen(porta, host,() => {
+    console.log(`O servidor está executando em https://${host}:${porta}`);
+});
