8330de934a1b44586d5e80d7c66f8c48def491bf
Jovedii <jovedigui@gmail.com>
2024-06-16 12:18:52 -0300

    ultimo

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(session({
     resave: true,
     saveUninitialized: true,
     cookie: {
-        maxAge: 1000 * 60 * 30
+        maxAge: 1000 * 60 * 60* 30
     }
 }));
 app.use(cookieParser());
